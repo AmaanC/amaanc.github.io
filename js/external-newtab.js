ee22aa5dbe6e542897adb0e995e994cdefa43e8f
Amaan Cheval <amaan.cheval@gmail.com>
2019-05-14 10:57:42 +0530

    deploy: Add more resources

--- js/external-newtab.js
+++
@@ -1,8 +0,0 @@
-// Load external links in a new tab
-var links = document.body.getElementsByTagName("a");
-
-for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-   if (links[i].hostname !== window.location.hostname) {
-      links[i].target = '_blank';
-   }
-}
